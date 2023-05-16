aa08810832783e9b0cfe661008823ee14f59f5c6
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-05-16 17:11:15 +0200

    finito

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express");
 
+const express = require("express");
+require("dotenv").config();
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -12,8 +14,8 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", movieHandlers.getMovies);
+app.get("/api/users/:id", movieHandlers.getMovieById);
 
 app.listen(port, (err) => {
   if (err) {
