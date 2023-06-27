aa0517b5b3fddddac7d9b731eab72f373618a165
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-06-27 11:38:59 +0200

    Express 04bis🔧 Valider la saisie utilisateur

--- app.js
+++ app.js
@@ -1,20 +1,93 @@
-const express = require("express");
+require("dotenv").config();
 
+// Importation du module Express et création de l'application
+const express = require("express");
 const app = express();
 
+// Middleware pour parser le corps des requêtes en JSON
+app.use(express.json());
+
+// Définition du port
 const port = 5000;
 
+// Configuration de dotenv pour charger les variables d'environnement depuis le fichier .env
+require("dotenv").config();
+
+// ----
+// Définition d'une fonction de route pour l'accueil
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
+// Définition de la route "/" avec la fonction welcome
 app.get("/", welcome);
 
+// Définition d'une fonction de route pour l'accueil avec nom personnalisé
+const welcomeName = (req, res) => {
+  res.send(`Welcome ${req.params.name}`);
+};
+// Définition de la route "/users/:name" avec la fonction welcomeName
+app.get("/users/:name", welcomeName);
+
+//------------
+
+// Importation des gestionnaires de routes pour les films depuis le fichier movieHandlers.js
 const movieHandlers = require("./movieHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+// -- Valider saisie 
+const { validateMovie } = require("./validators.js");
+const { validateUser  } = require("./validators.js");
+
+// Définition des routes pour les opérations CRUD sur les films
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMoviesById);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+
+const userHandlers = require("./userHandlers");
+const { hashPassword } = require("./auth.js");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+// app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post("/api/users", validateUser, userHandlers.postUser);
+app.put("/api/users/:id", hashPassword, userHandlers.updateUsers);
+
+// const loginHandlers = require("./loginHandlers");
+// app.post("/api/login", loginHandlers.postLogin);
+
+
+//🗝️ ----------Authentification avec JWT ------
+const isItDwight = (req, res) => {
+  if (req.body.email === "mara@lechat.com" && req.body.password === "123456") {
+    res.send("Credentials are valid");
+  } else {
+    res.sendStatus(401);
+  }
+};
+app.post("/api/login", isItDwight);
+
+
+
+const verifyPassword = (req, res) => {
+  res.send(req.user);
+}
+
+//--
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+//🗝️ ----------Authentification avec JWT ------
+
+
+//-----------
+
+// Démarrage du serveur et écoute sur le port spécifié
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
---
+++ userHandlers.js
@@ -0,0 +1,122 @@
+// CRUD
+//   CREATE --> post
+//   READ --->> get
+//   UPADATE --> PUT
+//   DELETe 
+
+const database = require("./database");
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language, hashedPassword } =
+    req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language, hashedPassword) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language, hashedPassword]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
+
+
+const getUsers = (req, res) => {
+  const initialSql = "select id, firstname, lastname, email, city, language from users";
+  const where = [];
+
+  // Construction de la requête SQL en ajoutant des conditions WHERE dynamiquement
+  const query = where.reduce(
+    (sql, { column, operator }, index) =>
+      `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+    initialSql
+  );
+
+  // Récupération des valeurs des conditions WHERE
+  const values = where.map(({ value }) => value);
+
+  // Exécution de la requête SQL avec les valeurs
+  database
+    .query(query, values)
+    .then(([users]) => {
+      // Envoi des utilisateurs récupérés en tant que réponse JSON
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      // Envoi d'une réponse d'erreur en cas de problème lors de la récupération des données de la base de données
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+  
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserByEmailWithPasswordAndPassToNext = (req, res) => {
+    const { email, pseudo } =
+      req.body;
+  
+    database
+      .query(
+        "INSERT INTO login( email, password) VALUES (?, ?)",
+        [ email , password]
+      )
+      .then(([result]) => {
+        res.location(`/api/login/${result.insertId}`).Status(201).send("Credentials are valid");
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(401).send("Error email or password");
+      });
+  };
+
+
+  const updateUsers = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {firstname, lastname, email, city, language, hashedPassword } = req.body;
+  
+    database
+      .query(
+        "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ?, hashedPassword = ? where id = ?",
+        [firstname, lastname, email, city, language, hashedPassword, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("🛸Error editing the users");
+      });
+  };
+
+  module.exports = {
+    postUser,
+    getUsers,
+    getUsersById,
+    updateUsers,
+    getUserByEmailWithPasswordAndPassToNext
+  };
